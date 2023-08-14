c0175f789b6bcfcf3fbe2fad5c7e5556a65ac2b3
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-08-14 10:59:13 +0900

    add: init folders

---
+++ src/components/layouts/HomeLayout.tsx
@@ -0,0 +1,43 @@
+import React, { FC, ReactNode } from 'react';
+import { Center, Container, Stack, Title } from '@mantine/core';
+
+import Header from './Header/Header';
+// import Footer from './Footer/Footer';
+
+const tabs = [
+  {
+    link: '/home',
+    label: 'トップページ'
+  },
+  {
+    link: '/portfolio',
+    label: 'Portfolio'  
+  },
+  {
+    link: '/tips',
+    label: 'Tips'
+  },
+  {
+    link: '/techfinder',
+    label: 'TechFinder'
+  },
+]
+
+type MainLayoutProps = {
+  children: ReactNode;
+};
+
+
+const HomeLayout: FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header {...{ tabs }} />
+      <Container className="pt-5 pb-10">
+        <Stack spacing="xl">{children}</Stack>
+      </Container>
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default HomeLayout;

1a3350094d4a81d4045aa4bad89c0f190436c809
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-08-16 16:27:52 +0900

    add; headline

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,9 @@
+import { UserInfoIcons } from '@/components/UserHeadLine';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <UserInfoIcons />
       <h1>portfolio page!</h1>
     </main>
   )

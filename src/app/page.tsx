3459f522852b55b28fd6141ef9a13da3201e01f5
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-08-19 15:04:46 +0900

    slillsのhoveractions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
 "use client";
 
-import Image from 'next/image'
-import React, { useEffect } from "react";
-import axios from "axios";
-
 import { HeroContentLeft } from '@/components/TopHero';
 
 export default function Home() {
-
-  const url = "http://localhost/api/skill";
-
-  useEffect(()=>{
-    (async ()=>{
-      try{
-        const res = await axios.get(url);
-        console.log(res);
-        return;
-      }catch (e){
-        return e;
-      }
-    })();
-  },[]);
 
   return (
     <main className="flex flex-col items-center justify-between p-24">
--- src/components/UserSkills.tsx
+++ src/components/UserSkills.tsx
@@ -1,14 +1,52 @@
 "use client";
 
-import { Grid, Skeleton, Container } from '@mantine/core';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Skill } from "@/types";
 
-const child = <Skeleton height={140} radius="md" animate={ false } />;
+import { MantineProvider, Center, Group } from '@mantine/core';
+import { Grid, Container } from '@mantine/core';
 
 export function GridSkillsSection() {
+
+  const [skills,setSkills] = useState([]);
+  const url = "http://localhost/api/skill";
+
+  useEffect(()=>{
+    (async ()=>{
+      try{
+        const res = await axios.get(url);
+        setSkills(res.data.post);
+        return;
+      }catch (e){
+        return e;
+      }
+    })();
+  },[]);
+
   return (
-    <Container my="md">
-      <Grid>
-        <Grid.Col xs={12}>{child}</Grid.Col>
+    <Container my="md" className="bg-white p-8 pt-2 rounded-2xl">
+      <Center sx={(theme) => ({
+            height: '2.5rem',
+            backgroundImage: theme.fn.gradient(),
+            color: theme.white,
+          })} className="rounded-xl w-1/2 font-bold transform translate-x-1/2 drop-shadow-lg my-4">
+          My Skills
+      </Center>
+      <Grid className="gap-1 flex justify-between">
+          {/* ゆくゆくはプロフィールから自分が選んだものだけ（idとかでstate維持） */}
+          {skills.slice(0,14).map((skill : Skill)=>{
+            return (
+              <Grid.Col xs={1.5} className="bg-slate-100 py-3 rounded-2xl relative drop-shadow-lg hover:transform hover:duration-500 hover:-translate-y-5 hover:cursor-pointer">
+                <div key={skill.id} className="flex justify-center">
+                  <figure>
+                    <img src={ skill.image } alt="" className="w-12 object-cover"/>
+                  </figure>
+                  <p className="font-bold text-center text-xs absolute bottom-0 mt-2">{skill.name}</p>
+                </div>
+              </Grid.Col>
+            );
+          })}
       </Grid>
     </Container>
   );
